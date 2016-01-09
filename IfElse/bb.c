46e6eb1f68827f07724a8300b54739e3a624e56c
tanmim <shikder.tanmim@gmail.com>
2016-01-10 00:37:21 +0600

    bb.c added

---
+++ IfElse/bb.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a;
+    a=5;
+
+    if(a==1){
+        printf("saturday");
+
+    }
+    else if(a==2) {
+        printf("sunday");
+}
+     else if(a==3) {
+        printf("monday");
+}
+     else if(a==4) {
+        printf("tuesday");
+}
+     else if(a==5) {
+        printf("wednesday");
+}
+     else if(a==6) {
+        printf("thursday");
+}
+     else if(a==7) {
+        printf("friday");
+}
+    else{
+
+        printf("not found");
+
+    }
+
+
+    return 0;
+
+
+
+}
